a6507ce73c627c1dfdd58c8aa3221cf2fd327944
Muzammil803 <syedmuzammil691@gmail.com>
2022-06-03 07:17:09 +0500

    assignment done

---
+++ app.js
@@ -0,0 +1,321 @@
+// assignment 04
+// chap 09 and 10 
+// assignment 04
+// question 01
+
+
+// var city = prompt("enter you city name")
+// if(city==="karachi"){
+//     document.write("<b>welcome to karachi <b/>")
+// }
+
+// question 02
+
+// var gender = prompt("enter your gender")
+// if(gender==="male" ){
+// document.write("<h1> hello sir<h1/>")    
+// }
+
+// if(gender==="female" ){
+//     document.write("<h1> hello mam<h1/>")    
+    
+// }
+
+//question 03
+
+// var color1 = prompt("enter color 01")
+// var color2 = prompt("enter color 02")
+// var color3 = prompt("enter color 03")
+// if(color1==="red" ){
+//     document.write("<h1>red = stop<h1/>")    
+//     }
+//     if(color2==="yellow" ){
+//         document.write("<h1>yellow = ready<h1/>")    
+//         }
+//         if(color3==="green" ){
+//             document.write("<h1>green = go<h1/>")    
+//             }
+                        
+//question 04
+
+// var fuel=+prompt("enter remaining fueal in ltr")
+// if(fuel<0.25){
+//     document.write("please refill your tank")
+// }
+// else{
+//     document.write("good")
+    
+// }
+
+// question 05
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// // User Input & Conditional Statement | JAVASCRIPT
+
+// // Page 3 of 4
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");}
+
+
+
+
+
+
+
+
+
+
+// var subjects = ["phys","chem","math","urdu","isl"];
+// var a = +prompt("enter no")
+// var result = subjects.splice(a,1)
+// console.log(result)
+
+
+//QUESTION NO 06
+
+// var totalmarks= 300
+// var obtainmarks=+prompt("enter obtained marks in three subjects")
+// var percent = obtainmarks/totalmarks*100
+
+// document.write("<h1> Total marks :  </h1>" + totalmarks + "</br>" + "<h1> Obtain Marks :  </h1>" + obtainmarks + "</br>" + "<h1> Percentage :  </h1>" + percent )
+// if(percent>=80){
+//     document.write("<h1> GRADE :A*  </h1>"+"</br>" + "<h1> EXCELLENT  </h1>")
+// }
+// else{}
+// if(percent<80 && percent>70 ){
+//     document.write("<h1> GRADE : A  </h1>"+
+//         "</br>" + "<h1> GOOD  </h1>")
+
+// }else{}
+// if(percent<70 &&percent>60 ){
+//     document.write("<h1> GRADE : B  </h1>"+"</br>" + "<h1> OKAY </h1>")
+
+// }else{}if(percent<60 ){
+//     document.write("<h1> GRADE : FAIL  </h1>"+"</br>" + "<h1> NEED MORE EFFORT </h1>")
+
+// }else{}
+
+// question 07
+// var a =8
+// var num = +prompt("guess a number between 1 to 10")
+// if(num===a){
+//     document.write("BINGO you won")
+// }
+
+// if(num===++a){
+//     document.write(" you are close")
+// }
+
+//question 08
+
+// var a = +prompt("enter number")
+// var b=a/3
+// if (b!=){alert("true")}
+
+//question 10
+
+// var t = +prompt("enter temprature")
+// if(t>40){
+//     document.write("its too hot")
+// }
+// if(t<40&& t>30){
+//     document.write("todays whether is normal")
+// }
+// if(t<30 && t>20){
+//     document.write("its cold")
+// }
+// if(t<20 ){
+//     document.write("its too cool")
+// }
+
+
+// var a = +prompt("enter first number")
+// var o = prompt("enter operation")
+// var b = +prompt("enter  second number")
+// if(o== "+" ){document.write(a+b)}
+// if(o== "-" ){document.write(a-b)}
+// if(o== "*" ){document.write(a*b)}
+// if(o== "/" ){document.write(a/b)}
+
+//chap 12 & 13
+
+//question 4
+
+// var a = prompt("enter a vowel")
+// if(a=="a"){
+//     alert("true")
+// }else if(a=="e"){alert("true")}else if(a=="i"){alert("true")}else if(a=="o"){alert("true")}else if(a=="u"){alert("true")}
+// else{alert("false")}
+
+
+//question no 5
+
+// var p="muzammil"
+// var a =prompt("enter password")
+// if(a==""){alert("enter password plz")}
+// else if(a==="muzammil"){alert("correct")}
+// else{alert("wrong password")}
+
+
+//question 6
+
+// var arr=prompt("enter");
+// var res = arr.reverse
+// if (arr === res){
+//     alert("true")
+// }
+// else if{}
+//     alert("false")
+// var checked = arr.split 
+
+//TABLE MAKER
+
+// var a =+prompt("enter")
+// for( var  i= 1; i <=20; ++i){
+// document.write(a + " X " + i + " = " + a*i + "<br>");
+// }
+
+
+// QUESTION 01
+//     var a=prompt("enter name")
+//     if(a=="A-Z"){alert("uper case")}
+//     // else if(a=="0-9"){alert("number")}
+//     // else if(a=="a-z"){alert("lower case")}
+// else{}
+
+
+//question no 2
+
+//  number =+ prompt("Enter a number: ");
+
+// //check if the number is even
+
+// if(number % 2 == 0) {
+
+// console.log("The number is even.");
+
+// }
+
+// // if the number is odd
+
+// else {
+
+// console.log("The number is odd.");
+
+// }
+
+// question no 7 
+
+// var time = +prompt("enter time")
+
+
+// if ( time >=0000 && time<1200){
+//     alert("GOOD MORNING")
+// }else if(time>=1200 && time<1700){
+//     alert("GOOD AFTERNOON")
+// }else if(time>=1700 && time<2100){
+//     alert("GOOD EVENING")
+// }else if(time>=2100 && time<2359){
+//     alert("GOOD NIGHT")
+// }
+
+//        CHAPTER NO 14 to 16
+
+//  arr=["HSC","SSC","HEC","BSC","MPHIL","MASTER"]
+
+//  document.write("<h1>QUALIFICATIONS </h1>" + "</br>" + arr[0] + "</br>" + arr[1]+ "</br>" +arr[2]+ "</br>" +arr[3]+ "</br>" +arr[4]+ "</br>" +arr[5] )
+// // 
+
+
+
+// // question 08
+//  a = ["muzammil" ,"hamza","hammad"]
+// b = [340,420,300]
+// document.write(
+//     "name:"+ a[0] + "score"+ b[0] + "percent"+ (b[0]/500)*100 +"</br>"
+// +    "name:"+ a[1] + "score"+ b[1] + "percent"+ (b[1]/500)*100 +"</br>"
+// +    "name:"+ a[2] + "score"+ b[2] + "percent"+ (b[2]/500)*100 +"</br>"
+ 
+
+//     )
+
+////////////////////////////////////////////////////////
+
+// color = ["red","blue"]
+
+// document.write(color)
+// // color.push("dskdvslh")
+// // document.write(color)
+
+// var ask1 = prompt("enter beggining color")
+
+//  color.unshift(ask1)
+// document.write("</br>"+ color)
+
+
+// var ask2 = prompt("enter last color")
+
+//  color.push(ask2)
+// document.write("</br>"+ color)
+
+
+//  color.shift()
+// document.write("</br>"+ color)
+
+
+// var ask3 = +prompt("in which index u want to add color")
+// var ask4 = prompt(" which color u want to add ")
+
+//  color.splice(ask3,0,ask4)
+// document.write("</br>"+ color)
+
+
+//////////////////////////////////////////
+// const fruits = [3,8,4,0,8,1,4,2,3,9];
+// fruits.sort();
+// document.write(fruits)
+
+// /////////////////////////////////
+// cities =["karachi","lahore","quetta","peshawar"]
+
+// document.write("selected cities  :" + cities)
+//   var a=  cities.slice(1,3)
+// document.write( "</br>"+ "selected cities : " + a)
